fe8ec69514c857c8ed53c53462bddcdaa4d4a210
llwslc <llwslc@gmail.com>
2020-12-03 23:44:47 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  sqlite: {
+    path: 'db'
+  },
+
+  fullNode: 'https://api.trongrid.io/',
+  privateKey: '0000000000000000000000000000000000000000000000000000000000000001',
+
+  useProxy: true,
+
+  klinesUrl: 'https://api.binancezh.pro/api/v1/klines'
+};
