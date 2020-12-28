d37c6c080c27e550544dd3906039c8bfc19ce5bb
llwslc <llwslc@gmail.com>
2020-12-29 00:12:46 +0800

    Update sqlite.js

--- sqlite.js
+++ sqlite.js
@@ -21,4 +21,82 @@ const dbOpen = async dbFile => {
       db.getSync = async (sql, param) => {
         return new Promise((reslove, reject) => {
           db.get(sql, param, (error, res) => {
+            if (error) return reject(error);
+            reslove(res ? [res] : []);
+          });
+        });
+      };
+      db.allSync = async (sql, param) => {
+        return new Promise((reslove, reject) => {
+          db.all(sql, param, (error, res) => {
+            if (error) return reject(error);
+            reslove(res);
+          });
+        });
+      };
+      db.runSync = async (sql, param) => {
+        return new Promise((reslove, reject) => {
+          db.run(sql, param, function (error) {
+            if (error) return reject(error);
+            reslove({ lastID: this.lastID, changes: this.changes });
+          });
+        });
+      };
+      db.execSync = async sql => {
+        return new Promise((reslove, reject) => {
+          db.exec(sql, error => {
+            if (error) return reject(error);
+            reslove(true);
+          });
+        });
+      };
+      reslove(db);
+    });
+  });
+};
+
+const db = async dbName => {
+  try {
+    const dbFile = path.join(dbPath, `${dbName}.db`);
+    const dbObj = await dbOpen(dbFile);
+    const klines = await dbObj.getSync(`SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'klines'`);
+    if (klines.length == 0) {
+      /*
+          [
+            1499040000000,      // Open time 开盘时间
+            "0.01634790",       // Open 开盘价
+            "0.80000000",       // High 最高价
+            "0.01575800",       // Low 最低价
+            "0.01577100",       // Close 收盘价(当前K线未结束的即为最新价)
+            "148976.11427815",  // Volume 成交量
+            1499644799999,      // Close time 收盘时间
+            "2434.19055334",    // Quote asset volume 成交额
+            308,                // Number of trades 成交笔数
+            "1756.87402397",    // Taker buy base asset volume 主动买入成交量
+            "28.46694368",      // Taker buy quote asset volume 主动买入成交额
+            "17928899.62484339" // 请忽略该参数
+          ]
+      */
+      await dbObj.runSync(`CREATE TABLE "klines" (
+        "id"	INTEGER NOT NULL,
+        "openTime"	INTEGER NOT NULL UNIQUE,
+        "open"	TEXT NOT NULL,
+        "high"	TEXT NOT NULL,
+        "low"	TEXT NOT NULL,
+        "close"	TEXT NOT NULL,
+        "volume"	TEXT NOT NULL,
+        "closeTime"	INTEGER NOT NULL,
+        "QuoteAssetVolume"	TEXT NOT NULL,
+        "numberOfTrades"	INTEGER NOT NULL,
+        "takerBuyBaseAssetVolume"	TEXT NOT NULL,
+        "takerBuyQuoteAssetVolume"	TEXT NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT)
+      );`);
+    }
+    return dbObj;
+  } catch (error) {
+    logger.error(`DB Error:`, error.message ? error.message : error);
+  }
+};
+
 module.exports = db;
