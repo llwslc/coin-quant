6fd1f218890ee8a4efbf51f5192a40e6615d4884
llwslc <llwslc@gmail.com>
2021-02-08 05:06:13 +0800

    Update config.js

--- front/src/config.js
+++ front/src/config.js
@@ -1,16 +1,20 @@
 const env = process.env.REACT_APP_ENV;
 
 const Config = {
-  backend: '',
+  backend: '/api/klines',
   localStorage: {
     favKey: 'favorites'
+  },
+  binanceUrl: {
+    base: 'https://www.binance.com/zh-CN/trade/',
+    query: '?layout=pro'
   }
 };
 
 const devConfig = {};
 if (env === 'development') {
   devConfig.env = env;
-  devConfig.backend = 'localhost:8080';
+  devConfig.backend = 'http://localhost:8080/api/klines';
 }
 
 export default Object.assign(Config, devConfig);
