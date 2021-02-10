33f138ca2d98a175a2f88bab1f582f121e87f0e6
llwslc <llwslc@gmail.com>
2021-02-11 03:53:47 +0800

    Update dayUp.js

--- server/quant/dayUp.js
+++ server/quant/dayUp.js
@@ -12,48 +12,100 @@ let db = {};
 const main = async () => {
   db = await sqlite(config.sqlite.dklines);
 
-  const openTime = 0; // new Date('2021-01-01 00:00:00').getTime();
-  const symbol = 'BTCUSDT';
-  const klines = await db.allSync(`SELECT * FROM klines WHERE symbol = '${symbol}' AND openTime > ${openTime};`);
-  console.log(klines.length);
-  const uOdAll = { up: 0, down: 0, d: 'down' };
+  const openTime = new Date('2021-01-01 00:00:00').getTime();
+  const klines = await db.allSync(`SELECT * FROM klines WHERE openTime > ${openTime};`);
+  const uOdAll = {};
   const uOdByDay = {};
+
   for (const d of klines) {
+    const symbol = d.symbol;
+    if (!uOdByDay[symbol]) {
+      uOdByDay[symbol] = {};
+      uOdAll[symbol] = { up: 0, down: 0, d: 'ep' };
+    }
+    const uOd = uOdByDay[symbol];
+    const uOdA = uOdAll[symbol];
     const openTime = d.openTime;
     const day = `周${new Date(openTime).getDay() + 1}`;
-    if (!uOdByDay[day]) {
-      uOdByDay[day] = { up: 0, down: 0 };
+    if (!uOd[day]) {
+      uOd[day] = { up: 0, down: 0 };
     }
-    let up = uOdByDay[day].up;
-    let down = uOdByDay[day].down;
+    let up = uOd[day].up;
+    let down = uOd[day].down;
 
     const open = Number(d.open);
     const close = Number(d.close);
     if (open < close) {
       up++;
-      uOdAll.up++;
+      uOdA.up++;
     } else {
       down++;
-      uOdAll.down++;
+      uOdA.down++;
     }
 
-    uOdByDay[day] = { up, down };
+    uOd[day] = { up, down };
   }
 
   for (const s in uOdByDay) {
-    uOdByDay[s].d = 'down';
-    if (uOdByDay[s].up > uOdByDay[s].down) {
-      uOdByDay[s].d = 'up';
+    const uOd = uOdByDay[s];
+    for (const d in uOd) {
+      uOd[d].d = 'ep';
+      if (uOd[d].up > uOd[d].down) {
+        uOd[d].d = 'up';
+      }
+      if (uOd[d].up < uOd[d].down) {
+        uOd[d].d = 'down';
+      }
+    }
+    if (uOdAll[s].up > uOdAll[s].down) {
+      uOdAll[s].d = 'up';
+    }
+    if (uOdAll[s].up < uOdAll[s].down) {
+      uOdAll[s].d = 'down';
     }
   }
-  if (uOdAll.up > uOdAll.down) {
-    uOdAll.d = 'up';
+
+  const showSymbol = ['BTCUSDT', 'ETHUSDT', 'LINKUSDT'];
+  for (const s of showSymbol) {
+    console.log(s, uOdAll[s]);
+    const sortDay = Object.keys(uOdByDay[s]).sort();
+    for (const d of sortDay) {
+      console.log(d, uOdByDay[s][d]);
+    }
   }
 
-  console.log(symbol, uOdAll);
-  const sortDay = Object.keys(uOdByDay).sort();
+  const uOdByDayRes = {};
+  for (const s in uOdByDay) {
+    const uOd = uOdByDay[s];
+    for (const d in uOd) {
+      if (!uOdByDayRes[d]) {
+        uOdByDayRes[d] = { upCoins: 0, downCoins: 0, epCoins: 0, d: 'down' };
+      }
+      if (uOd[d].d === 'up') {
+        uOdByDayRes[d].upCoins++;
+      }
+      if (uOd[d].d === 'down') {
+        uOdByDayRes[d].downCoins++;
+      }
+      if (uOd[d].d === 'ep') {
+        uOdByDayRes[d].epCoins++;
+      }
+    }
+  }
+
+  console.log('ALL COINS');
+  for (const d in uOdByDayRes) {
+    uOdByDayRes[d].d = 'ep';
+    if (uOdByDayRes[d].upCoins > uOdByDayRes[d].downCoins) {
+      uOdByDayRes[d].d = 'up';
+    }
+    if (uOdByDayRes[d].upCoins < uOdByDayRes[d].downCoins) {
+      uOdByDayRes[d].d = 'down';
+    }
+  }
+  const sortDay = Object.keys(uOdByDayRes).sort();
   for (const d of sortDay) {
-    console.log(d, uOdByDay[d]);
+    console.log(d, uOdByDayRes[d]);
   }
 };
 
