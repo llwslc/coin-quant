ff2045e2c4663b3b8e9cb5e454309555cb8d0fea
llwslc <llwslc@gmail.com>
2021-03-05 04:41:18 +0800

    Update demarkSequential.js

--- server/quant/demarkSequential.js
+++ server/quant/demarkSequential.js
@@ -15,41 +15,115 @@ const main = async () => {
   const openTime = 0; //new Date('2021-01-01 00:00:00').getTime();
   const symbol = 'BTCUSDT';
   const klines = await db.allSync(`SELECT * FROM klines WHERE symbol = '${symbol}' and openTime > ${openTime};`);
-  const {data} =  await axios.get(config.klinesUrl, {
+  const { data } = await axios.get(config.klinesUrl, {
     params: { symbol, interval: '1d', limit: 1 }
   });
-  const [curKlines = []] = data
+  const [curKlines = []] = data;
 
-  klines.push({openTime: curKlines[0],
+  klines.push({
+    openTime: curKlines[0],
     open: curKlines[1],
     high: curKlines[2],
     low: curKlines[3],
     close: curKlines[4],
-    volume:curKlines[5],
+    volume: curKlines[5]
   });
 
-  const DAY = 9
-  const BEFORE_DAY = 4
+  for (const k of klines) {
+    k.open = Number(k.open);
+    k.close = Number(k.close);
+    k.high = Number(k.high);
+    k.low = Number(k.low);
+  }
 
-  const upPoints = [];
-  const downPoints = [];
-  for (let i = (DAY + BEFORE_DAY); i < klines.length; i++) {
-    if ( klines[i].close < klines[i-4].high) {
-      return
+  const DAY = 9;
+  const BEFORE_DAY = 4;
+
+  const topPoints = [];
+  const confirmTopPoints = [];
+  const bottomPoints = [];
+  const confirmBottomPoints = [];
+
+  const findTop = _arr => {
+    for (let i = 0, iLen = _arr.length - BEFORE_DAY; i < iLen; ++i) {
+      if (_arr[i].close > _arr[i + BEFORE_DAY].close) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const findConfirmTop = _arr => {
+    const idx = _arr.length - 1;
+    if (_arr[idx].high > _arr[idx - 2].high && _arr[idx].high > _arr[idx - 3].high) {
+      if (_arr[idx - 1].high > _arr[idx - 2].high && _arr[idx - 1].high > _arr[idx - 3].high) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const findBottom = _arr => {
+    for (let i = 0, iLen = _arr.length - BEFORE_DAY; i < iLen; ++i) {
+      if (_arr[i].close < _arr[i + BEFORE_DAY].close) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const findConfirmBottom = _arr => {
+    const idx = _arr.length - 1;
+    if (_arr[idx].low < _arr[idx - 2].low && _arr[idx].low < _arr[idx - 3].low) {
+      if (_arr[idx - 1].low < _arr[idx - 2].low && _arr[idx - 1].low < _arr[idx - 3].low) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const printArr = _arr => {
+    return;
+
+    console.log(`===========`);
+    _arr.map(_ => {
+      console.log(_.id, new Date(_.openTime).toLocaleDateString(), _.close, _.low);
+    });
+    console.log(`===========`);
+  };
+
+  for (let i = DAY + BEFORE_DAY; i < klines.length; i++) {
+    const arr = klines.slice(i - (DAY + BEFORE_DAY), i);
+    if (findTop(arr)) {
+      topPoints.push(klines[i - 1]);
+      if (findConfirmTop(arr)) {
+        confirmTopPoints.push(klines[i - 1]);
+        printArr(arr);
+      }
+    }
+    if (findBottom(arr)) {
+      bottomPoints.push(klines[i - 1]);
+      if (findConfirmBottom(arr)) {
+        confirmBottomPoints.push(klines[i - 1]);
+      }
     }
   }
 
-  console.log('upPoints');
-  for (const d of upPoints) {
-    console.log(
-      new Date(d.openTime).toLocaleDateString(),
-    );
+  console.log('topPoints');
+  for (const d of topPoints) {
+    console.log(new Date(d.openTime).toLocaleDateString());
   }
-  console.log('downPoints');
-  for (const d of downPoints) {
-    console.log(
-      new Date(d.openTime).toLocaleDateString(),
-    );
+  console.log('confirmTopPoints');
+  for (const d of confirmTopPoints) {
+    console.log(new Date(d.openTime).toLocaleDateString());
+  }
+  console.log('bottomPoints');
+  for (const d of bottomPoints) {
+    console.log(new Date(d.openTime).toLocaleDateString());
+  }
+  console.log('confirmBottomPoints');
+  for (const d of confirmBottomPoints) {
+    console.log(new Date(d.openTime).toLocaleDateString());
   }
 };
 
