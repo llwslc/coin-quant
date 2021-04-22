da820891e34046760c9a344cacbb068cb29a0423
llwslc <llwslc@gmail.com>
2021-04-23 04:21:30 +0800

    Update index.js

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -4,6 +4,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import Fallback from '../components/Fallback';
 const Home = React.lazy(() => import('../components/Home'));
 const Help = React.lazy(() => import('../components/Help'));
+const Td9 = React.lazy(() => import('../components/Td9'));
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <React.Suspense fallback={<Fallback />}>
         <Switch>
           <Route path="/help" component={Help} />
+          <Route path="/td9" component={Td9} />
           <Route component={Home} />
         </Switch>
       </React.Suspense>
