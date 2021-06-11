2b8e43fd53b35455f0c6beb247076b0a0d5356c5
llwslc <llwslc@gmail.com>
2021-06-11 18:22:38 +0800

    exchanges

---
+++ server/quant/exchange.js
@@ -0,0 +1,11 @@
+/**
+ * TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX binance
+ * TMuA6YqfCeX8EhbfYEg5y7S4DqzSJireY9 binance cold wallet address
+ * TWd4WrZ9wn84f5x1hZhL4DHvk738ns5jwb binance cold wallet address
+ * TNaRAoLUyYEV2uF7GUrzSjRQTU8v5ZJ5VR huobi
+ * TXFBqBbqJommqZf7BV8NNYzePh97UmJodJ bitfinex
+ * TNCmcTdyrYKMtmE1KU2itzeCX76jGm5Not poloniex
+ * TBA6CypYJizwA9XdC7Ubgc5F1bxrQ7SqPt gate
+ * TYDzsYUEpvnYmQk4zGP9sWWcTEd2MiAtW6 ftx
+ * TWtqXzfx8vWf7gnhM1HKKHo7zvwRA65NdQ sbf
+ */
