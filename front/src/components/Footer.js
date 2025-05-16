03cc9e3a839eb135837600e584223460026b7990
llwslc <llwslc@gmail.com>
2025-05-16 23:37:23 +0800

    Update Footer.js

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 
 const { Link } = Typography;
 
-const FooterMain = styled.div`
+const FooterMain = styled.div``;
+
+const LinkStyle = styled.div`
   display: flex;
   justify-content: center;
   margin: 100px auto 40px;
@@ -13,13 +15,22 @@ const FooterMain = styled.div`
   }
 `;
 
+const CopyrightStyle = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   return (
     <FooterMain>
-      <Link href="#/">Home</Link>
-      <Link href="#/about">About</Link>
-      <Link href="#/td9">TD9</Link>
-      <Link href="#/help">Help</Link>
+      <LinkStyle>
+        <Link href="#/">Home</Link>
+        <Link href="#/about">About</Link>
+        <Link href="#/td9">TD9</Link>
+        <Link href="#/help">Help</Link>
+      </LinkStyle>
+
+      <CopyrightStyle>© 2025 北京超木子商贸中心 All rights reserved</CopyrightStyle>
     </FooterMain>
   );
 }
